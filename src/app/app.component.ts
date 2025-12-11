a4db8d8e3ed58dd4d73c340de2e96e2b911c1619
suyash@25 <suyashsrivastava209@gmail.com>
2025-12-12 01:25:36 +0530

    input field added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'helixonGLobalPrivateLimitedFrontend';
+  name:string='';
+  email:string='';
+  number:string='';
 }
